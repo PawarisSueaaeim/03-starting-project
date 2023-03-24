68f448dd61b59420863b7e842c5893b5608ee1b5
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-03-24 17:06:12 +0700

    added automate testbotton

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -11,5 +12,33 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello world!!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+
+  test("render text test this is text description!!!",() => {
+    render(<Greeting />);
+
+    const paragrahpElement = screen.getByText("this is text description!!!");
+    expect(paragrahpElement).toBeInTheDocument();
+  });
+
+  test("render text test: change text", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText("change text");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('render this is text description!!! not on click', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText("this is text description!!!");
+    expect(outputElement).toBeNull();
   });
 });
