1e6387aafbe09ca39fd10e7fcb32b46d4943b5b7
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-03-27 10:26:46 +0700

    finish testing intro

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe("Async conponent test menu list", () => {
     test('renders menu list',async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1' , title: 'test'}]
+        });
         render(<Async/>)
 
         const menuListElement = await screen.findAllByRole('listitem');
